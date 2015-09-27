fb1bcc3a3d70f269fba816cb3bedd7ed64fab1a2
Sumit <sumitmrsc@gmail.com>
2015-09-27 08:20:58 -0700

    Spring Boot with Spring Data JPA using H2 Database. H2 Database Web console is configured

---
+++ SpringRest1/src/main/java/demo/SpringRest1Application.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRest1Application.class, args);
+    }
+}
---
+++ SpringRest1/src/main/java/demo/WebConfig.java
@@ -0,0 +1,19 @@
+package demo;
+
+// This bean is used to configure H2 database`
+
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
